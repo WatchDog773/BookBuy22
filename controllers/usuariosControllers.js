b737e2b9941d8b34abeee83a3dd74a0e43aedbf6
JrMartinez-Zambrano <51100407+JrMartinez-Zambrano@users.noreply.github.com>
2020-07-16 20:18:10 -0600

    Agregando los endpoints para actualizar el usuario

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -34,3 +34,75 @@ exports.formularioIniciarSesion =  (req, res, next) =>{
 
   res.render("iniciar_sesion", { layout: "auth", messages });
 };
+
+
+
+
+exports.formularioVerUsuario =  async (req, res, next) =>{
+    const usuario = res.locals.usuario;
+    const mensajes = [];
+
+    try {
+        // Variable que almacena todos los proyectos que existem
+        const usuarios = await Usuario.findAll({
+            where:{
+                    id: usuario.id
+            } 
+        });
+        // Luego renderizo la vista que mostrará todos los proyectos que existen
+        res.render("ver_usuario", { usuarios } );
+ 
+     } catch (error) 
+     {
+         mensajes.push({error: "Error al obtener los datos del usuario, favor reintentar",
+         type: "alert-warning"
+         });
+         res.render("ver_usuario", mensajes);
+     }
+  };
+
+
+
+  
+  // Actualizar los datos de un proyecto
+  exports.actualizarUsuario = async (req, res, next) =>{
+    const {username, fullname, email, age, phone, address} = req.body;
+    const usuario = res.locals.usuario;
+
+
+
+    const mensajes = [] ;
+
+    // Verificar si el nombre del proyecto es enviado
+    if (!username){
+        mensajes.push({
+            error: "El nombre del usario no puede ser vacío!",
+            type: "alert-waning",
+        });
+    }
+
+    if (!email){
+        mensajes.push({
+            error: "El correo electrónico no puede estar vacío!",
+            type: "alert-waning",
+        });
+    }
+
+if (mensajes.length){
+
+  res.render("ver_usuario",{
+      mensajes,
+  });
+}else{
+   await Usuario.update(
+      {username, fullname, email, age, phone, address },
+      {
+          where:{
+            id: usuario.id, 
+          },
+      }
+  );
+  res.redirect("/ver_usuario" );
+}
+};
+
