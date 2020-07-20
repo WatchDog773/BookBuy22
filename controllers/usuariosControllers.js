a16f917f17f76c24cd8cb78c2a254669be204707
JrMartinez-Zambrano <51100407+JrMartinez-Zambrano@users.noreply.github.com>
2020-07-19 21:44:54 -0600

    Formateando el código en las distintas vistas

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -1,14 +1,14 @@
 // Importar el modelo 
 const Usuario = require("../models/Usuario");
 
-exports.formularioCrearCuenta = (req, res, next) =>{
-    res.render("registrarse", { layout: "auth"} );
+exports.formularioCrearCuenta = (req, res, next) => {
+    res.render("registrarse", { layout: "auth" });
 };
 
-exports.crearCuenta = async (req, res, next) =>{
+exports.crearCuenta = async (req, res, next) => {
     // Obtener los daos de la nueva cuenta
     // Obtenerlos por destructuring
-    const {username, email, password}= req.body;
+    const { username, email, password } = req.body;
 
     // Intentar crear el usuario
 
@@ -20,110 +20,110 @@ exports.crearCuenta = async (req, res, next) =>{
             password,
         });
         // Redireccionar el usuario al formulario de inicio de sesión, el lo tenia con redirect
-        res.render("iniciar_sesion", { layout: "auth"});
+        res.render("iniciar_sesion", { layout: "auth" });
     } catch (error) {
-        res.render("registrarse", { layout : "auth",error: error.message });
+        res.render("registrarse", { layout: "auth", error: error.message });
 
     }
 };
 
-exports.formularioIniciarSesion =  (req, res, next) =>{
-    
-  // Verificar si existe algún mensaje
-  const messages = res.locals.messages;
+exports.formularioIniciarSesion = (req, res, next) => {
 
-  res.render("iniciar_sesion", { layout: "auth", messages });
+    // Verificar si existe algún mensaje
+    const messages = res.locals.messages;
+
+    res.render("iniciar_sesion", { layout: "auth", messages });
 };
 
 
 
 
-exports.formularioVerUsuario =  async (req, res, next) =>{
+exports.formularioVerUsuario = async (req, res, next) => {
     const usuario = res.locals.usuario;
     const mensajes = [];
 
     try {
         // Variable que almacena todos los proyectos que existem
         const usuarios = await Usuario.findAll({
-            where:{
-                    id: usuario.id
-            } 
+            where: {
+                id: usuario.id
+            }
         });
         // Luego renderizo la vista que mostrará todos los proyectos que existen
-        res.render("ver_usuario", { usuarios } );
- 
-     } catch (error) 
-     {
-         mensajes.push({error: "Error al obtener los datos del usuario, favor reintentar",
-         type: "alert-warning"
-         });
-         res.render("ver_usuario", mensajes);
-     }
-  };
+        res.render("ver_usuario", { usuarios });
+
+    } catch (error) {
+        mensajes.push({
+            error: "Error al obtener los datos del usuario, favor reintentar",
+            type: "alert-warning"
+        });
+        res.render("ver_usuario", mensajes);
+    }
+};
 
 
 
-  
-  // Actualizar los datos de un proyecto
-  exports.actualizarUsuario = async (req, res, next) =>{
-    const {username, fullname, email, age, phone, address} = req.body;
+
+// Actualizar los datos de un proyecto
+exports.actualizarUsuario = async (req, res, next) => {
+    const { username, fullname, email, age, phone, address } = req.body;
     const usuario = res.locals.usuario;
 
 
 
-    const mensajes = [] ;
+    const mensajes = [];
 
     // Verificar si el nombre del proyecto es enviado
-    if (!username){
+    if (!username) {
         mensajes.push({
             error: "El nombre del usario no puede ser vacío!",
             type: "alert-waning",
         });
     }
 
-    if (!email){
+    if (!email) {
         mensajes.push({
             error: "El correo electrónico no puede estar vacío!",
             type: "alert-waning",
         });
     }
 
-if (mensajes.length){
+    if (mensajes.length) {
 
-  res.render("ver_usuario",{
-      mensajes,
-  });
-}else{
-   await Usuario.update(
-      {username, fullname, email, age, phone, address },
-      {
-          where:{
-            id: usuario.id, 
-          },
-      }
-  );
-  res.redirect("/ver_usuario" );
-}
+        res.render("ver_usuario", {
+            mensajes,
+        });
+    } else {
+        await Usuario.update(
+            { username, fullname, email, age, phone, address },
+            {
+                where: {
+                    id: usuario.id,
+                },
+            }
+        );
+        res.redirect("/ver_usuario");
+    }
 };
 
 
 
-exports.controlUsuarios =  async (req, res, next) =>{
+exports.controlUsuarios = async (req, res, next) => {
     const usuario = res.locals.usuario;
     const mensajes = [];
 
     try {
         // Variable que almacena todos los proyectos que existem
-        const usuariosAll= await Usuario.findAll({
+        const usuariosAll = await Usuario.findAll({
         });
         // Luego renderizo la vista que mostrará todos los proyectos que existen
-        res.render("control_usuarios", { layout: "admin",usuariosAll } );
-     } catch (error) 
-     {
-         mensajes.push({error: "Error al obtener los datos del los usuario, favor reintentar",
-         type: "alert-warning"
-         });
-         res.render("control_usuarios", mensajes);
-     }
-  };
+        res.render("control_usuarios", { layout: "admin", usuariosAll });
+    } catch (error) {
+        mensajes.push({
+            error: "Error al obtener los datos del los usuario, favor reintentar",
+            type: "alert-warning"
+        });
+        res.render("control_usuarios", mensajes);
+    }
+};
 
