52e6ff47d0370f7e5e377af63fa756fbde2463eb
Emanuel Aguilar <Emanuel Aguilar@DESKTOP-M6GR8FE>
2020-07-25 23:54:02 -0600

    Implementacion correo

--- helpers/email.js
+++ helpers/email.js
@@ -6,3 +6,19 @@ const mailTrapConfig = require("../config/email");
 const hbs = require("express-handlebars");
 const fs = require("fs");
 const path = require("path");
+
+// Realizar el envío del correo electrónico mediante nodemailer
+// hacia Mailtrap
+// https://nodemailer.com/about/
+exports.enviarCorreo = async (opciones) => {
+    // Crear nuestro transportador SMTP reutilizable
+    const transporter = nodemailer.createTransport({
+      host: mailTrapConfig.host,
+      port: mailTrapConfig.port,
+      secure: false, // true for 465 port, false for other ports
+      auth: {
+        user: mailTrapConfig.user,
+        pass: mailTrapConfig.pass,
+      },
+    });
+  
