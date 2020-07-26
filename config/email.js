e678a07163f13b1544fb28e20a4e5948e3d76fb7
Emanuel Aguilar <Emanuel Aguilar@DESKTOP-M6GR8FE>
2020-07-26 00:38:35 -0600

    module.exports

--- config/email.js
+++ config/email.js
@@ -1,1 +1,8 @@
 // Configuracion para la conexion con Mailtrap
+
+module.exports = {
+    user: process.env.USERMAILTRAP,
+    pass: process.env.PASSMAILTRAP,
+    host: process.env.HOSTMAILTRAP,
+    port: process.env.PORTMAILTRAP,
+};
